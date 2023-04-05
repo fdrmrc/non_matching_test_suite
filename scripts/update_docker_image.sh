83b86dfd03140b5bccd5c603719e01269ee37473
Luca Heltai <luca.heltai@sissa.it>
2023-04-05 09:40:37 +0300

    Change tag according to type of workflow.

--- scripts/update_docker_image.sh
+++ scripts/update_docker_image.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 test -f docker/Dockerfile || echo \
     "You must run this script from the top level directory of the repository."
-# Run this script from the top level directory
-docker build . -f docker/Dockerfile --tag ghcr.io/fdrmrc/non_matching_test_suite:latest
-docker push ghcr.io/fdrmrc/non_matching_test_suite:latest
 
+TAG=latest
+
+if [[ "$1" != "" ]]; then 
+    TAG=$1
+fi
+echo "Will build and push ghcr.io/fdrmrc/non_matching_test_suite:$TAG"
+
+# Run this script from the top level directory
+docker build . -f docker/Dockerfile \
+    --tag ghcr.io/fdrmrc/non_matching_test_suite:$TAG
+
+docker push ghcr.io/fdrmrc/non_matching_test_suite:$TAG
