cb18707752c51f274bc94d82ec63d914211f2d21
Marco Feder <marco.feder@sissa.it>
2023-08-23 23:03:36 +0200

    modify run_all_tests.sh for codeocean

--- scripts/run_all_tests.sh
+++ scripts/run_all_tests.sh
@@ -1,58 +1,55 @@
 #!/bin/bash
 
 cd ../build/examples/
-export PARAM_DIR=../../parameters/
+export PARAM_DIR=../../data/
+export BINARY_DIR=../../code/build/examples/
+
+#Runs everything in Release mode.
 
 #Tables 1,2,3
-./lm_1d2d_disk $PARAM_DIR/lm_1d2d_disk.smooth.prm
-./nitsche_1d2d_smooth_disk
-./cutfem_1d2d_smooth_disk
+printf "Table 1\n"
+./$BINARY_DIR/lm_1d2d_disk $PARAM_DIR/lm_1d2d_disk.smooth.prm
+printf "Table 2\n"
+./$BINARY_DIR/nitsche_1d2d_smooth_disk
+printf "Table 3\n"
+./$BINARY_DIR/cutfem_1d2d_smooth_disk
 
 
 
 #Tables 4,5,6
-./lm_1d2d_flower $PARAM_DIR/lm_1d2d_flower.smooth.prm
-./nitsche_1d2d_smooth_flower
-./cutfem_1d2d_smooth_flower
+printf "Table 4\n"
+./$BINARY_DIR//lm_1d2d_flower $PARAM_DIR/lm_1d2d_flower.smooth.prm
+printf "Table 5\n"
+./$BINARY_DIR//nitsche_1d2d_smooth_flower
+printf "Table 6\n"
+./$BINARY_DIR//cutfem_1d2d_smooth_flower
 
 
 
 #Tables 7,8,9
-./lm_1d2d_disk $PARAM_DIR/lm_1d2d_disk.non_smooth.prm
-./nitsche_1d2d_non_smooth_disk
-./cutfem_1d2d_non_smooth_disk
+printf "Table 7\n"
+./$BINARY_DIR/lm_1d2d_disk $PARAM_DIR/lm_1d2d_disk.non_smooth.prm
+printf "Table 8\n"
+./$BINARY_DIR/nitsche_1d2d_non_smooth_disk
+printf "Table 9\n"
+./$BINARY_DIR/cutfem_1d2d_non_smooth_disk
 
 
 
 #Tables 10,11,12
-./lm_2d3d $PARAM_DIR/lm_2d3d.smooth_sphere.prm
-./nitsche_2d23_smooth_disk
-./cutfem_2d_3d_smooth_sphere
+printf "Table 10\n"
+./$BINARY_DIR/lm_2d3d $PARAM_DIR/lm_2d3d.smooth_sphere.prm
+printf "Table 11\n"
+./$BINARY_DIR/nitsche_2d23_smooth_disk
+printf "Table 12\n"
+./$BINARY_DIR/cutfem_2d_3d_smooth_sphere
 
 
 
 #Tables 13,14,15
-./lm_2d3d $PARAM_DIR/lm_2d3d.non_smooth_sphere.prm
-./nitsche_2d23_non_smooth_disk
-./cutfem_2d_3d_non_smooth_sphere
-
-
-# if [ "$1" == "-h" ]; then 
-#         echo Usage: $0 [debug]
-#         echo
-#         echo Will run all tests in release mode, in serial.
-#         echo If [debug] is specified, then the programs are run in debug mode.
-#         echo The output is saved in the current directory.
-#         exit 0
-# fi
-
-# POSTFIX=""
-
-# if [ "$1" == "debug" ]; then 
-#     POSTFIX=".g"
-# fi
-
-# for prm in parameters/*prm; do
-#     exe=$(echo "$(basename $prm)" | cut -d'.' -f1)
-#     ./scripts/non_matching_test_suite.sh $exe$POSTFIX $prm
-# done
+printf "Table 13\n"
+./$BINARY_DIR/lm_2d3d $PARAM_DIR/lm_2d3d.non_smooth_sphere.prm
+printf "Table 14\n"
+./$BINARY_DIR/nitsche_2d23_non_smooth_disk
+printf "Table 15\n"
+./$BINARY_DIR/cutfem_2d_3d_non_smooth_sphere
